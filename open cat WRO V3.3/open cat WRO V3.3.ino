7fc34721ed2ddbacadbb4450633ce93921d9c740
LOfangQuanZachary <88233381+LOfangQuanZachary@users.noreply.github.com>
2021-08-13 17:25:16 +0800

    added a random int

--- open cat WRO V3.3/open cat WRO V3.3.ino
+++ open cat WRO V3.3/open cat WRO V3.3.ino
@@ -1,9 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-
+const int hello = 1;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(LED_BUILTIN, HIGH);
+
 }
